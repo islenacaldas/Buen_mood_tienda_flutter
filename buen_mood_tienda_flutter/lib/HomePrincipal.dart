2474db5005239af8557d9722dd843210a56ff374
IslenaCaldas <islenacaldas@gmail.com>
2026-08-08 16:53:28 -0500

    Agrega funcionalidad al carrito

--- buen_mood_tienda_flutter/lib/HomePrincipal.dart
+++ buen_mood_tienda_flutter/lib/HomePrincipal.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'Principal.dart';
 
 List<Map> productos = [
   {"nombre": "Falda", "precio": "\$90.000", "foto": "assets/falda.png"},
   {
-    "nombre": "guantes para alzar pesas",
+    "nombre": "Guantes para alzar pesas",
     "precio": "\$60.000",
     "foto": "assets/guante.png",
   },
@@ -14,48 +13,100 @@ List<Map> productos = [
     "foto": "assets/tennis.png",
   },
   {
-    "nombre": "top cruzado dama",
+    "nombre": "Top cruzado dama",
     "precio": "\$25.000",
     "foto": "assets/top.png",
   },
 ];
 
-class HomePrincipal extends StatelessWidget {
+class HomePrincipal extends StatefulWidget {
   const HomePrincipal({super.key});
+
+  @override
+  State<HomePrincipal> createState() => _HomePrincipalState();
+}
+
+class _HomePrincipalState extends State<HomePrincipal> {
+  List<Map> carrito = [];
+
+  void agregarAlCarrito(Map producto) {
+    setState(() {
+      carrito.add(producto);
+    });
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('${producto["nombre"]} fue agregado al carrito'),
+        duration: const Duration(seconds: 2),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Column(
-          children: [
-            Icon(Icons.fitness_center_outlined),
-            SizedBox(width: 8),
-            Text("Buen Mood"),
-          ],
-        ),
+        title: const Text("Buen Mood"),
         backgroundColor: Colors.pinkAccent,
         foregroundColor: Colors.black,
+        actions: [
+          Stack(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.shopping_cart, color: Colors.black),
+              ),
+              if (carrito.isNotEmpty)
+                Positioned(
+                  right: 5,
+                  top: 5,
+                  child: Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: const BoxDecoration(
+                      color: Colors.black,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Text(
+                      '${carrito.length}',
+                      style: const TextStyle(color: Colors.white, fontSize: 12),
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ],
       ),
       body: ListView.separated(
         itemCount: productos.length,
         separatorBuilder: (BuildContext context, int index) {
-          return Divider();
+          return const Divider();
         },
         itemBuilder: (BuildContext context, int index) {
           return Card(
             child: ListTile(
-              title: Text(productos[index]["nombre"] + " "),
-              subtitle: Text("Precio " + productos[index]["precio"]),
+              title: Text(productos[index]["nombre"]),
+              subtitle: Text("Precio ${productos[index]["precio"]}"),
+              leading: Image.asset(
+                productos[index]["foto"],
+                width: 60,
+                height: 60,
+                fit: BoxFit.cover,
+              ),
               trailing: Container(
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                   color: Color(0xFFE91E63),
                   shape: BoxShape.circle,
                 ),
-                padding: EdgeInsets.all(8),
-                child: Icon(Icons.add_shopping_cart, color: Colors.white),
+                child: IconButton(
+                  onPressed: () {
+                    agregarAlCarrito(productos[index]);
+                  },
+                  icon: const Icon(
+                    Icons.add_shopping_cart,
+                    color: Colors.white,
+                  ),
+                ),
               ),
-              leading: Image(image: NetworkImage(productos[index]["foto"])),
             ),
           );
         },
